eb010d8b04a95fc9aaa7426a25fab32f544358ec
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-05-03 15:27:15 -0300

    Category Class and records

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorApp.Pages {
     public class IndexModel : PageModel {
-        public void OnGet() { }
+
+        public List<Category> Categories { get; set; } = [];
+
+        public async Task OnGet() {
+            await Task.Delay(5000);
+            for (int i = 1; i <= 100; i++)
+                Categories.Add(new Category(i, $"Categoria {i}", 1.25 * i));
+        }
 
         public void OnPost() { }
     }
+
+    public record Category(int Id, string Title, double Price);
 }
